6b4bf03a5e9fa8f128d49f053f58f421beb43277
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-02 01:38:07 -0400

    Adjust topBar style

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -19,14 +19,14 @@ const TopBar = () => {
         <IconButton className={styles["topBar-refresh-button"]} text="refresh" src={getAssetPath('folder-demo.svg')}/>
         <IconButton className={styles["topBar-settings-button"]} text="settings" src={getAssetPath('folder-demo.svg')}/>
       </div>
-      <hr />
+      <div className={styles["topBar-vertical-divider"]}></div>
       <div className={styles["topBar-control-column"]}>
-      <IconButton className={styles["topBar-pause-button"]} text="pause" src={getAssetPath('folder-demo.svg')} />
-      <IconButton className={styles["topBar-stop-button"]} text="stop" src={getAssetPath('folder-demo.svg')}/>
+        <IconButton className={styles["topBar-pause-button"]} text="pause" src={getAssetPath('folder-demo.svg')} />
+        <IconButton className={styles["topBar-stop-button"]} text="stop" src={getAssetPath('folder-demo.svg')}/>
       </div>
-      <hr />
+      <div className={styles["topBar-vertical-divider"]}></div>
       <div className={styles["topBar-spoof-column"]}>
-        <p>       Spoof: [__________________]</p>
+        <p>[c] (_) [Spoof Url...                        ]</p>
       </div>
     </div>
   );
