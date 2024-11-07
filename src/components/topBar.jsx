4fe05a3405cd87ec1c1a7949fa53bda5e5b37332
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-07 14:44:54 -0500

    Added better icons

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -21,27 +21,27 @@ const TopBar = ({
   return (
     <div className={styles["topBar-root"]}>
       <div className={styles["topBar-file-column"]}>
-        <IconButton className={styles["topBar-open-button"]} text="Open" src={getAssetPath('folder-demo.svg')} onClick={ () => { console.error('asd'); } }/>
+        <IconButton className={styles["topBar-open-button"]} text="Open" src={getAssetPath('folder.svg')} onClick={ () => { console.error('asd'); } }/>
         <IconButton
           className={styles["topBar-refresh-button"]}
           text="Refresh"
-          src={getAssetPath('folder-demo.svg')}
+          src={getAssetPath('refresh-double.svg')}
           onClick={async () => {
             const files = await invoke("scan_directory");
             setSwfFiles(files);
           }}
         />
-        <IconButton className={styles["topBar-settings-button"]} text="Settings" src={getAssetPath('folder-demo.svg')} onClick={ () => { console.error('dfg'); } }/>
+        <IconButton className={styles["topBar-settings-button"]} text="Settings" src={getAssetPath('settings.svg')} onClick={ () => { console.error('dfg'); } }/>
       </div>
       <div className={styles["topBar-vertical-divider"]}></div>
       <div className={styles["topBar-control-column"]}>
-        <IconButton className={styles["topBar-pause-button"]} text="Pause" src={getAssetPath('folder-demo.svg')} />
-        <IconButton className={styles["topBar-stop-button"]} text="Stop" src={getAssetPath('folder-demo.svg')}/>
+        <IconButton className={styles["topBar-pause-button"]} text="Pause" src={getAssetPath('pause.svg')} />
+        <IconButton className={styles["topBar-stop-button"]} text="Stop" src={getAssetPath('square.svg')}/>
       </div>
       <div className={styles["topBar-vertical-divider"]}></div>
       <div className={styles["topBar-spoof-column"]}>
         <input className={styles["topBar-spoof-checkbox"]} type='checkbox' onChange={(e) => { setGlobalSpoofEnabled(e.target.checked)}}/>
-        <img className={styles["topBar-spoof-icon"]} src={getAssetPath('globe-demo.svg')}/>
+        <img className={styles["topBar-spoof-icon"]} src={getAssetPath('globe.svg')}/>
         <input
           className={styles["topBar-spoof-url-textbox"]}
           type='text'
