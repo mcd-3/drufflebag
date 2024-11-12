394440bc666f30b36500e707c19e16d0029c3686
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-12 14:56:51 -0500

    Added ruffle player support

--- src/pages/emulator/EmulationContent.jsx
+++ src/pages/emulator/EmulationContent.jsx
@@ -1,12 +1,44 @@
+import { useEffect } from 'react';
 import { BroadcastChannel } from 'broadcast-channel';
+import "./../../styles/Emulation.css";
 
 function EmulationContent() {
   const bc = new BroadcastChannel("drufflebag_channel");
 
+  const mountRuffle = () => {
+    const script = document.createElement("script");
+
+    script.src = "ruffle-core/ruffle.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
+  const play = () => {
+    window.RufflePlayer = window.RufflePlayer || {};
+    window.addEventListener("load", (event) => {
+        const ruffle = window.RufflePlayer.newest();
+        const player = ruffle.createPlayer();
+
+        player.config = {
+          autoplay: "auto",
+          // and so on...
+        }
+
+        const container = document.getElementById("ruffle-container");
+        container.appendChild(player);
+        player.load("/the-worlds-hardest-game.swf");
+    });
+  }
+
+  useEffect(() => {
+    mountRuffle();
+    play();
+  }, []);
+
   return (
-    <div className="container">
-        <p>hello world!</p>
-        <button onClick={() => { bc.postMessage("close_ruffle"); }}>Close me</button>
+    <div id="ruffle-container" className="container">
+
     </div>
   );
 }
