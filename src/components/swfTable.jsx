f651b5e59dbdbdfdf21d13e338dfea4736939c7c
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-12 23:12:08 -0500

    Replace table with react-table

--- src/components/swfTable.jsx
+++ src/components/swfTable.jsx
@@ -1,10 +1,82 @@
 import styles from '../styles/components/swfTable.module.css';
+import { formatBytes } from './../utils/bytes.js';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
 
-const SwfTable = ({children}) => {
+const SwfTable = ({ swfFiles, setSelectedSwfPath }) => {
+  const columnHelper = createColumnHelper()
+
+  const columns = [
+    columnHelper.accessor('avm', {
+      cell: info => <img src="public/avm_unknown.svg" width={32} height={32} />,
+      header: 'AVM',
+    }),
+    columnHelper.accessor('path', {
+      cell: info => info.getValue().split('/').pop(),
+      header: 'Name',
+    }),
+    columnHelper.accessor('type', {
+      cell: info => 'Game',
+      header: 'Type',
+    }),
+    columnHelper.accessor('size', {
+      cell: info => formatBytes(info.getValue()),
+      header: 'Size',
+    }),
+    columnHelper.accessor('lp', {
+      cell: info => "June 23, 2024",
+      header: 'Last Played',
+    }),
+    columnHelper.accessor('status', {
+      cell: info => "Playable",
+      header: 'Status',
+    }),
+    columnHelper.accessor('url', {
+      cell: info => "https://ducklife.com",
+      header: 'URL Spoof',
+    }),
+  ];
+
+  const table = useReactTable({
+    data: swfFiles,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   return (
     <div className={styles['swfTable-root']}>
       <table>
-        {children}
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id} onClick={() => setSelectedSwfPath(row.original.path)}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/pages/main/MainContent.jsx
+++ src/pages/main/MainContent.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { formatBytes } from './../../utils/bytes.js';
 import "./../../styles/App.css";
 
 import TopBar from "../../components/topBar";
 import SwfTable from "../../components/swfTable";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
 
 function MainContent() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -28,50 +33,7 @@ function MainContent() {
         />
       </div>
       <div className="swf-content-table">
-        <SwfTable>
-          <thead>
-            <tr>
-              <th>AVM</th>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Size</th>
-              <th>Last Played</th>
-              <th>URL Spoof</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              swfFiles.map((swf) => {
-                return (
-                  <tr onClick={() => { setSelectedSwfPath(swf.path) }}>
-                    <td><img src="public/avm_unknown.svg" width={32} height={32} /></td>
-                    <td>{swf.path.split('/').pop()}</td>
-                    <td>Animation</td>
-                    <td>{formatBytes(swf.size)}</td>
-                    <td>June 23, 2024</td>
-                    <td>ducklife.com</td>
-                  </tr>
-                )
-              })
-            }
-            <tr>
-              <td><img src="public/avm2.svg"  width={32} height={32} /></td>
-              <td><button onClick={() => greet()}>That Gravity Game</button></td>
-              <td>Game (Select)</td>
-              <td>543 kb</td>
-              <td>November 4, 2024</td>
-              <td>moegames.com</td>
-            </tr>
-            <tr>
-              <td><img src="public/avm1.svg"  width={32} height={32} /></td>
-              <td><button onClick={() => greet()}>Red Ball</button></td>
-              <td>Game (Select)</td>
-              <td>123 kb</td>
-              <td>April 20, 2024</td>
-              <td>king.com</td>
-            </tr>
-          </tbody>
-        </SwfTable>
+        <SwfTable swfFiles={swfFiles} setSelectedSwfPath={setSelectedSwfPath}/>
       </div>
     </div>
   );
