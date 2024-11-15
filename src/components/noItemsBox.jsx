0ed7d6e2decb06647061f3f0fa6607451821876f
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-15 02:07:51 -0500

    Improved "no items" box styles

--- src/components/noItemsBox.jsx
+++ src/components/noItemsBox.jsx
@@ -3,9 +3,11 @@ import styles from '../styles/components/noItemsBox.module.css';
 const NoItemsBox = () => {
   return (
     <div className={styles['noItemsBox-container']}>
-      <h2 className={`${styles.row} ${styles['noItemsBox-header']}`}>No SWF Files Detected</h2>
-      <img className={`${styles.row} ${styles['noItemsBox-icon']}`} width={100} height={100} src="/src/assets/file-not-found.svg" />
-      <p className={`${styles.row} ${styles['noItemsBox-subtext']}`}>Click on "Open" to find some.</p>
+      <div className={styles['centered']}>
+        <h2 className={`${styles.row} ${styles['noItemsBox-header']}`}>No SWF Files Detected</h2>
+        <img className={`${styles.row} ${styles['noItemsBox-icon']}`} width={100} height={100} src="/src/assets/file-not-found.svg" />
+        <p className={`${styles.row} ${styles['noItemsBox-subtext']}`}>Click on "Open" to find some.</p>
+      </div>
     </div>
   );
 };
