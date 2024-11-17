f8852be62b86bc845a203aad0e542898b187b14e
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-11-17 16:35:34 -0500

    Small settings update

--- src/pages/settings/SettingsContent.jsx
+++ src/pages/settings/SettingsContent.jsx
@@ -2,6 +2,16 @@ function SettingsContent() {
   return (
     <div>
         <p>Settings coming soon!</p>
+        <h2>Experimental Features</h2>
+        <p>These features may not work as intended...Enable at your own risk!</p>
+        <div>
+          <input type="checkbox" id="url_spoof_enabled" name="url_spoof_enabled" />
+          <label htmlFor="url_spoof_enabled">URL Spoof Enabled</label>
+          < br/>
+          <button onClick={() => { console.log('Reset!') }} >Reset to Default</button>
+          <button onClick={() => { console.log('Saved!') }} >Save</button>
+          <button onClick={() => { console.log('Cancelled!') }} >Cancel</button>
+        </div>
     </div>
   );
 }
