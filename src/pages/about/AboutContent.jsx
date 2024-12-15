9b379e29324dbe30125c663b20d9533bd5fe74f1
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2024-12-14 19:49:14 -0500

    Add version permission for about

--- src/pages/about/AboutContent.jsx
+++ src/pages/about/AboutContent.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+import { getVersion } from '@tauri-apps/api/app';
+
 function AboutContent() {
+  const [appVersion, setAppVersion] = useState();
+
+  useEffect(() => {
+    getVersion().then((res) => {
+      setAppVersion(res);
+    });
+
+    return () => { }
+  }, [setAppVersion])
+
   return (
     <div>
-        <p>About</p>
+        <p>Drufflebag</p>
+        <p>Version: { appVersion }</p>
     </div>
   );
 }
