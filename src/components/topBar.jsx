ed81af11022f624c01a60bb55e1a00625111b38f
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2026-01-24 18:12:52 -0500

    Added topBar locale strings

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -22,6 +22,16 @@ import { getAsset } from './../utils/assets.js';
 import Swf from "../models/swf.js";
 import IconButton from './iconButton';
 import styles from './../styles/components/topBar.module.css';
+import { Locale } from "../locales/index.js";
+
+const {
+  ICON_BUTTON_ABOUT,
+  ICON_BUTTON_OPEN,
+  ICON_BUTTON_PLAY,
+  ICON_BUTTON_REFRESH,
+  ICON_BUTTON_SETTINGS,
+  ICON_BUTTON_STOP,
+} = Locale;
 
 /**
  * Bar that display a list of controls and options
@@ -105,14 +115,14 @@ const TopBar = ({
       <div className={styles["topBar-file-column"]}>
         <IconButton
           className={styles["topBar-open-button"]}
-          text="Open"
+          text={ ICON_BUTTON_OPEN }
           src={getAsset("ICN_FOLDER")}
           onClick={async () => {
             scanSwfDirectory();
           }} />
         <IconButton
           className={styles["topBar-refresh-button"]}
-          text="Refresh"
+          text={ ICON_BUTTON_REFRESH }
           src={getAsset("ICN_REFRESH")}
           onClick={async () => {
             const directory = getCachedDirectory();
@@ -127,7 +137,7 @@ const TopBar = ({
       <div className={styles["topBar-control-column"]}>
         <IconButton
           className={styles["topBar-pause-button"]}
-          text={ruffleOpen ? 'Stop' : 'Play'}
+          text={ruffleOpen ? ICON_BUTTON_STOP : ICON_BUTTON_PLAY}
           src={ruffleOpen ? getAsset("ICN_STOP") : getAsset("ICN_PLAY")}
           onClick={() => {
             if (ruffleOpen) {
@@ -147,12 +157,12 @@ const TopBar = ({
       <div className={styles["topBar-more-column"]}>
         <IconButton
             className={styles["topBar-settings-button"]}
-            text="Settings"
+            text={ ICON_BUTTON_SETTINGS }
             src={getAsset("ICN_SETTINGS")}
             onClick={ () => { openSettings() }} />
         <IconButton
           className={styles["topBar-about-button"]}
-          text="About"
+          text={ ICON_BUTTON_ABOUT }
           src={getAsset("ICN_ABOUT")}
           onClick={ () => { openAbout() }} />
       </div>
