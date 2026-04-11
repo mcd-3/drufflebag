dc7adfad6b78f05e825ad3f2b020770ab77df059
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2026-04-11 18:08:15 -0400

    Rework style imports for noItemsBox

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,6 +2,7 @@ import collapseStyles from './components/collapse.module.css';
 import contextMenuStyles from './components/contextMenu.module.css';
 import headerStyles from './components/header.module.css';
 import iconButtonStyles from './components/iconButton.module.css';
+import noItemsBoxStyles from './components/noItemsBox.module.css';
 
 export const collapse = {
   button: collapseStyles["collapse-button"],
@@ -24,4 +25,14 @@ export const iconButton = {
   container: iconButtonStyles["iconButton-root"],
   icon: iconButtonStyles.icon,
   text: iconButtonStyles.text,
+};
+
+export const noItemsBox = {
+  centered: noItemsBoxStyles.centered,
+  container: noItemsBoxStyles["noItemsBox-container"],
+  extra: noItemsBoxStyles["noItemsBox-extra"],
+  header: noItemsBoxStyles["noItemsBox-header"],
+  icon: noItemsBoxStyles["noItemsBox-icon"],
+  row: noItemsBoxStyles.row,
+  subtext: noItemsBoxStyles["noItemsBox-subtext"]
 };
